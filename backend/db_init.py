ca1de0b93ac37ce83d6259758d70e3547085603a
ShreeSub <rsl2171997@gmail.com>
2021-11-28 18:39:55 -0500

    updating db_init

--- backend/db_init.py
+++ backend/db_init.py
@@ -3,4 +3,8 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://bot:bot123@cluster0.xph5e.mongodb.net/feature-hunt?retryWrites=true&w=majority")
 db = client.get_database('feature-hunt')
 records = db.users
+feature_dict = [{'id': 2, 'text': 'feature-1', 'votes': 1, 'timestamp': '1234567', 'tags': ['tag1']}]
+product_input = {'name': 'name', 'description': 'description',
+                             'image_url': 'https://somrthing.gif/', 'users': ['user@email'], 'tags': ['tag1','tag2'], 'features': feature_dict}
+db.products.insert(product_input)
 product_records = db.products
